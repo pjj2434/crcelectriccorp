b8ed7b3faf488a4e85d3c3e69d2a9a5cd63ae0fd
pjj2434 <89330361+pjj2434@users.noreply.github.com>
2025-07-25 20:08:46 -0400

    SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/components/nav";
 import "./globals.css";
 import { useEffect, useState } from "react";
 import Footer from "@/components/footer";
-
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -27,6 +27,26 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        {/* HTML Meta Tags */}
+        <title>CRC Electric | Expert Electrical Services in Long Island, NY</title>
+        <meta name="description" content="CRC Electric provides licensed electrical services including lighting, panel upgrades, wiring, and installations across Nassau & Suffolk County. Free estimates!" />
+
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="https://crcelectriccorp.com/home" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="CRC Electric | Expert Electrical Services in Long Island, NY" />
+        <meta property="og:description" content="CRC Electric provides licensed electrical services including lighting, panel upgrades, wiring, and installations across Nassau & Suffolk County. Free estimates!" />
+        <meta property="og:image" content="/logo.PNG" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="crcelectriccorp.com" />
+        <meta property="twitter:url" content="https://crcelectriccorp.com/home" />
+        <meta name="twitter:title" content="CRC Electric | Expert Electrical Services in Long Island, NY" />
+        <meta name="twitter:description" content="CRC Electric provides licensed electrical services including lighting, panel upgrades, wiring, and installations across Nassau & Suffolk County. Free estimates!" />
+        <meta name="twitter:image" content="/logo.PNG" />
+      </Head>
       <body>
         <Navbar/>
         {children}
